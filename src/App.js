1b0c4e0f972ba17097367f85fc08ffa4d5914cb9
sariunar <rinchinovasariuna@gmail.com>
2023-05-27 20:23:59 +0800

    add comment form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [arr, setArr] = useState([]);
+  const [value, setValue] = useState('');
+
+  const result = arr.map((element, index) => {
+    return <p key={index} className='addCom'>{element}</p>;
+  });
+
+  function checkSpam() {
+    let comment = { value }.value;
+    comment = comment.replace(/viagra/gi, "***");
+    comment = comment.replace(/xxx/gi, "***");
+    return comment;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App" >
+      <form className="form">
+        <div className='form_greeting'>
+          <p>Hi</p>
+          <p>how are you?</p>
+          <div id="container"></div>
+        </div>
+        <div>{result}</div>
+        <div className="divComment">
+          <textarea name="comment" className="comment" value={value} onChange={event => setValue(event.target.value)}></textarea>
+          <input type="button" className="btn" value="Send" onClick={event => setArr([...arr, checkSpam()], setValue(''))} />
+        </div>
+      </form>
+    </div >
+  )
 }
 
 export default App;
